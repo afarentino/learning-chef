05fe0a095a30004b7c4967811cee3887c902709e
Tony Castrogiovanni <Tony.Castrogiovanni@sas.com>
2013-04-03 14:12:54 -0400

    Initial push of repository content - includes starting chef-repo, vagrantfile for vm, and several cookbooks/recipes

---
+++ cookbooks/aliases/metadata.rb
@@ -0,0 +1,8 @@
+name             'aliases'
+maintainer       'TNC Technolgies'
+maintainer_email 'anthonyC@ureach.com'
+license          'All rights reserved'
+description      'Installs/Configures aliases'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          'magic_shell', '~> 0.2.0'
